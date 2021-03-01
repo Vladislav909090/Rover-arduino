512b49bb0346635235e13b5216e69bddf183d917
Vladislav <amurchik2003@inbox.ru>
2021-03-01 23:57:24 +0300

    Джостик теперь может управлять скоростью моторов, а не только направлением. Изменена соответствующе программа на принятие сигнала

--- Arduino program/control_of_the_motors_via_bluetooth_successful/control_of_the_motors_via_bluetooth_successful.ino
+++ Arduino program/control_of_the_motors_via_bluetooth_successful/control_of_the_motors_via_bluetooth_successful.ino
@@ -6,18 +6,19 @@
 //                            4||3
 
  int parspeedperiod=50;
+ int speeed = 0;
  
- #define Motor1_1 42 //FR
+ #define Motor1_1 2 //FR EN
  #define Motor1_2 43 //FR
- #define Motor2_1 44 //CR
+ #define Motor2_1 3 //CR EN
  #define Motor2_2 45 //CR
- #define Motor3_1 46 //BR
+ #define Motor3_1 4 //BR EN
  #define Motor3_2 47 //BR
- #define Motor4_1 48 //BL
+ #define Motor4_1 5 //BL EN
  #define Motor4_2 49 //BL
- #define Motor5_1 50 //CL
+ #define Motor5_1 6 //CL EN
  #define Motor5_2 51 //CL
- #define Motor6_1 52 //FL
+ #define Motor6_1 7 //FL EN
  #define Motor6_2 53 //FL
 
 void setup()  {
@@ -28,163 +29,174 @@ void setup()  {
     pinMode(i, OUTPUT);
     digitalWrite (i, LOW);
   }
+  for (int i=2; i<=7;i++) {
+    analogWrite (i, 0);
+  }
 }
 
-void movewh(char orientation, int speed) { //функция с параметрами куда ехать и с какой скоростью (от 0 до 100)
+void movewh(char orientation, int speeed) { //функция с параметрами куда ехать и с какой скоростью (от 0 до 100)
   if (orientation == 'F') { //вперед
-    digitalWrite (Motor1_1, HIGH);
-    digitalWrite (Motor1_2, LOW); 
-    digitalWrite (Motor2_1, HIGH);
-    digitalWrite (Motor2_2, LOW); 
-    digitalWrite (Motor3_1, HIGH);
-    digitalWrite (Motor3_2, LOW); 
-    digitalWrite (Motor4_1, HIGH);
-    digitalWrite (Motor4_2, LOW); 
-    digitalWrite (Motor5_1, HIGH);
-    digitalWrite (Motor5_2, LOW);
-    digitalWrite (Motor6_1, HIGH);
-    digitalWrite (Motor6_2, LOW);
-    delay(speed);    
-  } else if (orientation == 'B') { //назад
-    digitalWrite (Motor1_1, LOW);
-    digitalWrite (Motor1_2, HIGH);  
-    digitalWrite (Motor2_1, LOW);
-    digitalWrite (Motor2_2, HIGH); 
-    digitalWrite (Motor3_1, LOW);
-    digitalWrite (Motor3_2, HIGH); 
-    digitalWrite (Motor4_1, LOW);
-    digitalWrite (Motor4_2, HIGH);
-    digitalWrite (Motor5_1, LOW);
-    digitalWrite (Motor5_2, HIGH); 
-    digitalWrite (Motor6_1, LOW);
-    digitalWrite (Motor6_2, HIGH);    
-    delay(speed);     
-  } else if (orientation == 'R') {  //вправо
-    digitalWrite (Motor1_1, LOW);
+    analogWrite(Motor1_1, speeed);
     digitalWrite (Motor1_2, HIGH); 
-    digitalWrite (Motor2_1, LOW);
+    analogWrite(Motor2_1, speeed);
     digitalWrite (Motor2_2, HIGH); 
-    digitalWrite (Motor3_1, LOW);
+    analogWrite(Motor3_1, speeed);
     digitalWrite (Motor3_2, HIGH); 
-    digitalWrite (Motor4_1, HIGH);
-    digitalWrite (Motor4_2, LOW); 
-    digitalWrite (Motor5_1, HIGH);
-    digitalWrite (Motor5_2, LOW); 
-    digitalWrite (Motor6_1, HIGH);
-    digitalWrite (Motor6_2, LOW); 
-    delay(speed);    
-  } else if (orientation == 'L') {  //влево
-    digitalWrite (Motor1_1, HIGH);
-    digitalWrite (Motor1_2, LOW);
-    digitalWrite (Motor2_1, HIGH);
-    digitalWrite (Motor2_2, LOW); 
-    digitalWrite (Motor3_1, HIGH);
-    digitalWrite (Motor3_2, LOW); 
-    digitalWrite (Motor4_1, LOW);
-    digitalWrite (Motor4_2, HIGH);  
-    digitalWrite (Motor5_1, LOW);
-    digitalWrite (Motor5_2, HIGH); 
-    digitalWrite (Motor6_1, LOW);
-    digitalWrite (Motor6_2, HIGH);  
-    delay(speed);     
-  } else if (orientation == 'I') { //вперед налево
-    digitalWrite (Motor1_1, HIGH);
-    digitalWrite (Motor1_2, LOW);
-    digitalWrite (Motor2_1, HIGH);
-    digitalWrite (Motor2_2, LOW); 
-    digitalWrite (Motor3_1, HIGH);
-    digitalWrite (Motor3_2, LOW); 
-    digitalWrite (Motor4_1, LOW);
-    digitalWrite (Motor4_2, LOW);  
-    digitalWrite (Motor5_1, HIGH);
-    digitalWrite (Motor5_2, LOW); 
-    digitalWrite (Motor6_1, HIGH);
-    digitalWrite (Motor6_2, LOW);
-    delay(speed);     
-  } else if (orientation == 'Y') { //вперед направо
-    digitalWrite (Motor1_1, HIGH);
-    digitalWrite (Motor1_2, LOW);
-    digitalWrite (Motor2_1, HIGH);
-    digitalWrite (Motor2_2, LOW); 
-    digitalWrite (Motor3_1, LOW);
-    digitalWrite (Motor3_2, LOW); 
-    digitalWrite (Motor4_1, HIGH);
-    digitalWrite (Motor4_2, LOW);
-    digitalWrite (Motor5_1, HIGH);
-    digitalWrite (Motor5_2, LOW); 
-    digitalWrite (Motor6_1, HIGH);
-    digitalWrite (Motor6_2, LOW);   
-    delay(speed);     
-  } else if (orientation == 'T') { //назад налево
-    digitalWrite (Motor1_1, LOW);
-    digitalWrite (Motor1_2, HIGH);
-    digitalWrite (Motor2_1, LOW);
-    digitalWrite (Motor2_2, HIGH); 
-    digitalWrite (Motor3_1, LOW);
-    digitalWrite (Motor3_2, HIGH); 
-    digitalWrite (Motor4_1, LOW);
-    digitalWrite (Motor4_2, HIGH);  
-    digitalWrite (Motor5_1, LOW);
-    digitalWrite (Motor5_2, HIGH); 
-    digitalWrite (Motor6_1, LOW);
-    digitalWrite (Motor6_2, LOW); 
-    delay(speed);     
-  } else if (orientation == 'N') { //назад направо
-    digitalWrite (Motor1_1, LOW);
-    digitalWrite (Motor1_2, LOW);
-    digitalWrite (Motor2_1, LOW);
-    digitalWrite (Motor2_2, HIGH); 
-    digitalWrite (Motor3_1, LOW);
-    digitalWrite (Motor3_2, HIGH); 
-    digitalWrite (Motor4_1, LOW);
-    digitalWrite (Motor4_2, HIGH);  
-    digitalWrite (Motor5_1, LOW);
-    digitalWrite (Motor5_2, HIGH); 
-    digitalWrite (Motor6_1, LOW);
+    analogWrite(Motor4_1, speeed);
+    digitalWrite (Motor4_2, HIGH); 
+    analogWrite(Motor5_1, speeed);
+    digitalWrite (Motor5_2, HIGH);
+    analogWrite(Motor6_1, speeed);
     digitalWrite (Motor6_2, HIGH);
-    delay(speed);     
-  } 
-    if (speed!=parspeedperiod){
-      digitalWrite (Motor1_1, LOW);
-      digitalWrite (Motor1_2, LOW);
-      digitalWrite (Motor2_1, LOW);
-      digitalWrite (Motor2_2, LOW); 
-      digitalWrite (Motor3_1, LOW);
-      digitalWrite (Motor3_2, LOW); 
-      digitalWrite (Motor4_1, LOW);
-      digitalWrite (Motor4_2, LOW);
-      digitalWrite (Motor5_1, LOW);
-      digitalWrite (Motor5_2, LOW); 
-      digitalWrite (Motor6_1, LOW);
-      digitalWrite (Motor6_2, LOW);
-      delay(parspeedperiod-speed); 
-    }
+     
+  } else if (orientation == 'B') { //назад
+    analogWrite(Motor1_1, speeed);
+    digitalWrite (Motor1_2, LOW);  
+    analogWrite(Motor2_1, speeed);
+    digitalWrite (Motor2_2, LOW); 
+    analogWrite(Motor3_1, speeed);
+    digitalWrite (Motor3_2, LOW); 
+    analogWrite(Motor4_1, speeed);
+    digitalWrite (Motor4_2, LOW);
+    analogWrite(Motor5_1, speeed);
+    digitalWrite (Motor5_2, LOW); 
+    analogWrite(Motor6_1, speeed);
+    digitalWrite (Motor6_2, LOW);  
+          
+  } else if (orientation == 'R') {  //вправо
+    analogWrite(Motor1_1, speeed);
+    digitalWrite (Motor1_2, LOW); 
+    analogWrite(Motor2_1, speeed);
+    digitalWrite (Motor2_2, LOW); 
+    analogWrite(Motor3_1, speeed);
+    digitalWrite (Motor3_2, LOW); 
+    analogWrite(Motor4_1, speeed);
+    digitalWrite (Motor4_2, HIGH); 
+    analogWrite(Motor5_1, speeed);
+    digitalWrite (Motor5_2, HIGH);
+    analogWrite(Motor6_1, speeed);
+    digitalWrite (Motor6_2, HIGH); 
+  
+  } else if (orientation == 'L') {  //влево
+    analogWrite(Motor1_1, speeed);
+    digitalWrite (Motor1_2, HIGH);  
+    analogWrite(Motor2_1, speeed);
+    digitalWrite (Motor2_2, HIGH); 
+    analogWrite(Motor3_1, speeed);
+    digitalWrite (Motor3_2, HIGH); 
+    analogWrite(Motor4_1, speeed);
+    digitalWrite (Motor4_2, LOW);
+    analogWrite(Motor5_1, speeed);
+    digitalWrite (Motor5_2, LOW); 
+    analogWrite(Motor6_1, speeed);
+    digitalWrite (Motor6_2, LOW);  
+   
+  } else if (orientation == 'I') { //вперед налево
+    analogWrite(Motor1_1, speeed);
+    digitalWrite (Motor1_2, HIGH); 
+    analogWrite(Motor2_1, speeed);
+    digitalWrite (Motor2_2, HIGH); 
+    analogWrite(Motor3_1, speeed);
+    digitalWrite (Motor3_2, HIGH); 
+    analogWrite(Motor4_1, 0);
+    analogWrite(Motor5_1, speeed);
+    digitalWrite (Motor5_2, HIGH);
+    analogWrite(Motor6_1, speeed);
+    digitalWrite (Motor6_2, HIGH); 
+    
+  } else if (orientation == 'Y') { //вперед направо
+    analogWrite(Motor1_1, speeed);
+    digitalWrite (Motor1_2, HIGH); 
+    analogWrite(Motor2_1, speeed);
+    digitalWrite (Motor2_2, HIGH); 
+    analogWrite(Motor3_1, 0); 
+    analogWrite(Motor4_1, speeed);
+    digitalWrite (Motor4_2, HIGH); 
+    analogWrite(Motor5_1, speeed);
+    digitalWrite (Motor5_2, HIGH);
+    analogWrite(Motor6_1, speeed);
+    digitalWrite (Motor6_2, HIGH);  
+   
+  } else if (orientation == 'T') { //назад налево
+    analogWrite(Motor1_1, speeed);
+    digitalWrite (Motor1_2, LOW);  
+    analogWrite(Motor2_1, speeed);
+    digitalWrite (Motor2_2, LOW); 
+    analogWrite(Motor3_1, speeed);
+    digitalWrite (Motor3_2, LOW); 
+    analogWrite(Motor4_1, speeed);
+    digitalWrite (Motor4_2, LOW);
+    analogWrite(Motor5_1, speeed);
+    digitalWrite (Motor5_2, LOW); 
+    analogWrite(Motor6_1, 0);
+   
+  } else if (orientation == 'N') { //назад направо
+    analogWrite(Motor1_1, 0); 
+    analogWrite(Motor2_1, speeed);
+    digitalWrite (Motor2_2, LOW); 
+    analogWrite(Motor3_1, speeed);
+    digitalWrite (Motor3_2, LOW); 
+    analogWrite(Motor4_1, speeed);
+    digitalWrite (Motor4_2, LOW);
+    analogWrite(Motor5_1, speeed);
+    digitalWrite (Motor5_2, LOW); 
+    analogWrite(Motor6_1, speeed);
+    digitalWrite (Motor6_2, LOW); 
+    
+  } if (orientation == 'S'){
+    analogWrite(Motor1_1, 0);
+    analogWrite(Motor2_1, 0);
+    analogWrite(Motor3_1, 0);
+    analogWrite(Motor4_1, 0);
+    analogWrite(Motor5_1, 0);
+    analogWrite(Motor6_1, 0);
+  }
 }
 
 void loop() { 
   if (Serial3.available()) {  
     char c = Serial3.read();
-    
+
   switch (c){
-      case 'X':movewh('Y',50);//вперед направо
+      case '9':speeed=255;Serial.println("255");
         break;
-      case 'P':movewh('N',50);//назад направо
+      case '8':speeed=230;Serial.println("220");
         break;
-      case 'V':movewh('T',50);//назад налево
+      case '7':speeed=205;Serial.println("190");
         break;
-      case 'B':movewh('I',50);//вперед налево
+      case '6':speeed=180;Serial.println("160");
         break;
-      case 'N':movewh('F',50);//вперед
+      case '5':speeed=155;Serial.println("120");
         break;
-      case 'M':movewh('B',50);//назад
+      case '4':speeed=130;Serial.println("90");
         break;
-      case 'Z':movewh('R',50);//вправо
+      case '3':speeed=105;Serial.println("70");
         break;
-      case 'L':movewh('L',50);//влево
+      case '2':speeed=80;Serial.println("50");
         break;
-      case 'W':movewh('F',10);//вперед
+      case '1':speeed=55;Serial.println("30");
         break;
-      case '0':movewh('F', 0);//стоп
-    }
+      case '0':speeed=30;Serial.println("10");
+        break;
+      case 'X':movewh('Y',speeed);Serial.println("Y");//вперед направо
+        break;
+      case 'P':movewh('N',speeed);Serial.println("N");//назад направо
+        break;
+      case 'V':movewh('T',speeed);Serial.println("T");//назад налево
+        break;
+      case 'B':movewh('I',speeed);Serial.println("I");//вперед налево
+        break;
+      case 'N':movewh('F',speeed);Serial.println("F");//вперед
+        break;
+      case 'M':movewh('B',speeed);Serial.println("B");//назад
+        break;
+      case 'Z':movewh('R',speeed);Serial.println("R");//вправо
+        break;
+      case 'L':movewh('L',speeed);Serial.println("L");//влево
+        break;
+      case 'W':movewh('F',100);Serial.println("F");//вперед
+        break;
+      case 'Q':movewh('S', 0);Serial.println("Q");//стоп
+    } 
   }
 }
--- Arduino program/test_program_with_djostik/test_program_with_djostik.ino
+++ Arduino program/test_program_with_djostik/test_program_with_djostik.ino
@@ -3,6 +3,10 @@
 #define pinY    A1  // ось Y джойстика
 #define swPin    2  // кнопка джойстика
 
+int generalsp;
+int dgeneralsp = 1000;//беру случайное число
+
+  
 void setup() {
   Serial.begin(230400);
   
@@ -14,41 +18,58 @@ void setup() {
  
 void loop() {
   boolean ledState = digitalRead(swPin);   
-  int X = analogRead(pinX);              
-  int Y = analogRead(pinY); 
-  int spX = abs(512-X)/512;
-  int spY = abs(512-Y)/512; 
-  int spD = abs(724-abs(sqrt(X*X+Y*Y)))/724;
-  int erproc = round (0.5*1023/100);  //погрешность измерения в проценте - 0.5% = 5 ед.   
-
+  int X = analogRead(pinY);//меняю координаты для того, чтобы оси XY на джостике располпгплись по-другому (вверх джостика в противоположной стороне от проводов)            
+  int Y = analogRead(pinX); 
+  float spX = ((512-(float)X)/512)*100;
+  spX = abs(spX);
+  float spY = ((512-(float)Y)/512)*100;
+  spY = abs(spY);
+  float spDR = floor(((724-sqrt((float)X*(float)X+(float)Y*(float)Y))/724)*100);
+  spDR = abs(spDR);
+  float spDL = floor(((724-sqrt((float)(1023-X)*(float)(1023-X)+(float)Y*(float)Y))/724)*100);
+  spDL = abs(spDL);
+  int erproc = floor (0.005*1023);  //погрешность измерения в проценте - 0.5% = 5 ед.   
+  //Serial.print("X:");Serial.println(X);
+  //Serial.print("Y:");Serial.println(Y);
+  int dX = map(X, 0, 1024, -512, 512);
+  int dY = map(Y, 0, 1024, -512, 512);
+  int dspX = map((int)spX, 0, 98, 0, 9);
+  int dspY = map((int)spY, 0, 98, 0, 9);
+  int dspDR = map((int)spDR, 0, 98, 0, 9);
+  int dspDL = map((int)spDL, 0, 98, 0, 9);
+  
   if (!ledState) { 
-         Serial.println("W");
-  } else if ((Y>-2*X)&&(Y<-0.5*X)) {
+         Serial.println("W");//нажатие кнопки
+  } else if (((-15-erproc)<=dY)&&(dY<=(-15+erproc)) && ((-15-erproc)<=dX)&&(dX<=(-15+erproc))) {
+         Serial.println("Q");//стсутствие действий
+  } else if ((dY>-2*dX)&&(dY<-0.5*dX)) {
+         generalsp = dspDL;
          Serial.println("P");//назад направо 
-         Serial.println(spD);
-  } else if ((Y>2*X)&&(Y<0.5*X)) {
+  } else if ((dY>2*dX)&&(dY<0.5*dX)) {
+         generalsp = dspDR;
          Serial.println("V");//назад налево
-         Serial.println(spD);
-  } else if ((Y>-0.5*X)&&(Y<-2*X)) {
+  } else if ((dY>-0.5*dX)&&(dY<-2*dX)) {
+         generalsp = dspDL;
          Serial.println("B");//вперед налево 
-         Serial.println(spD);
-  } else if ((Y>0.5*X)&&(Y<2*X)) {
+  } else if ((dY>0.5*dX)&&(dY<2*dX)) {
+         generalsp = dspDR;
          Serial.println("X");//вперед направо
-         Serial.println(spD);
-  } else if (Y>2*abs(X)) {
+  } else if ((dY>=-2*dX)&&(dY>=2*dX)) {
+         generalsp = dspY;
          Serial.println("N"); //вперед
-         Serial.println(spY);
-  } else if (Y<=-2*abs(X)){
+  } else if ((dY<=-2*dX)&&(dY<=2*dX)){
+         generalsp = dspY;
          Serial.println("M");//назад
-         Serial.println(spY);
-  } else if ((Y>=-0.5*X)&&(Y<=0.5*X)) {
+  } else if ((dY>=-0.5*dX)&&(dY<=0.5*dX)) {
+         generalsp = dspX;
          Serial.println("Z"); //вправо
-         Serial.println(spX);
-  } else if ((Y<=-0.5*X)&&(Y>=0.5*X)){
+  } else if ((dY<=-0.5*dX)&&(dY>=0.5*dX)){
+         generalsp = dspX;
          Serial.println("L");//влево
-         Serial.println(spX);
-  } else if ((496-erproc<=Y<=496+erproc)&&(496-erproc<=X<=496+erproc)) {
-         Serial.println("0");
   }
-  delay(1000);
+
+   if (generalsp != dgeneralsp) {
+    Serial.println(generalsp);
+    dgeneralsp = generalsp;
+   }
 }
